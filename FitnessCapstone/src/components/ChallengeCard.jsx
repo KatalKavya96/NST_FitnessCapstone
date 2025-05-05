2fc1f6cb8cae71970baae35efe68562427cd86f4
Kavya Katal <kavyakatal09@gmail.com>
2025-05-05 19:57:25 +0530

    Commit

--- FitnessCapstone/src/components/ChallengeCard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ChallengeCard = () => {
-  return (
-    <div>ChallengeCard</div>
-  )
-}
-
-export default ChallengeCard
--- FitnessCapstone/src/components/LeaderboardCard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const LeaderboardCard = () => {
-  return (
-    <div>LeaderboardCard</div>
-  )
-}
-
-export default LeaderboardCard
