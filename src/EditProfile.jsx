153174888865f964e034193b2e51ba18cc433258
Kavya Katal <kavyakatal09@gmail.com>
2025-08-01 23:30:43 +0530

    Commit

--- src/EditProfile.jsx
+++ src/EditProfile.jsx
@@ -129,7 +129,6 @@ const EditProfile = () => {
           </div>
 
           <form onSubmit={handleSubmit} className="space-y-6">
-            {/* Basic Info Section */}
             <section className="space-y-4">
               <h3 className="text-xl font-semibold">Basic Information</h3>
               <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
