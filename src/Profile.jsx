9c955dbb8f2ffa0de8ac6c52b131702972b6998f
Kavya Katal <kavyakatal09@gmail.com>
2025-08-10 06:31:57 +0530

    Commit

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -154,7 +154,6 @@ const Profile = () => {
               </div>
             )}
 
-            {/* Following Modal */}
             {showFollowing && (
               <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
                 <div className={`${isDarkMode ? 'bg-gray-800' : 'bg-white'} rounded-lg p-6 max-w-md w-full mx-4`}>
