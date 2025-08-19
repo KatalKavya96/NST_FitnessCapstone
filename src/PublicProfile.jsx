4778fe8b9eb0183b791ec2aef296e15d8f47449b
Kavya Katal <kavyakatal09@gmail.com>
2025-08-19 09:20:52 +0530

    Commit

--- src/PublicProfile.jsx
+++ src/PublicProfile.jsx
@@ -20,7 +20,6 @@ const PublicProfile = () => {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center py-10 px-4">
       <div className="w-full max-w-xl bg-white rounded-2xl shadow-lg overflow-hidden">
-        {/* Header */}
         <div className="bg-gradient-to-r from-indigo-600 to-purple-600 h-32 relative">
           <div className="absolute left-1/2 transform -translate-x-1/2 -bottom-12">
             <img
